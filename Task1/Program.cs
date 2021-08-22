80a6aeb29d3ab9ea5a329f2b3b714c6f52b98824
skripkalisa <skripkalisa.msk@ya.ru>
2021-08-23 02:57:33 +0300

    Added missing files

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GetInput();
+        }
+
+        private static void GetInput()
+        {
+            char[] operators = { '+', '-', '*', '/', '%' };
+            Console.WriteLine("Введите первое число:");
+            var a = CheckDouble();
+            Console.WriteLine("Введите один из операторов:");
+            foreach (var op in operators)
+                Console.Write($"{op} ");
+            Console.WriteLine();
+            var input = Console.ReadLine();
+            var oper = '+';
+            if (input != null)
+                try
+                {
+                    oper = input[input.IndexOfAny(operators)];
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Ошибка! Пустая строка вместо оператора. " + e.Message);
+                    Console.WriteLine("Использую оператор: '+'");
+                }
+
+            Console.WriteLine("Введите второе число:");
+            var b = CheckDouble();
+
+            Console.WriteLine(new Calc(a, oper, b).GetResult());
+        }
+
+        private static double CheckDouble()
+        {
+            var num = double.TryParse(Console.ReadLine(), out var dblNum);
+            if (num)
+                return dblNum;
+            Console.WriteLine("Введённые данные не являются числом. Использую значение: '0'.");
+            return 0;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            var calc = new Calc(Logger);
+            calc.GetInput();
+        }
+    }
+}
